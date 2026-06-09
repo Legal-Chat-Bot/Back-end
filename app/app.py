a786f657c73cfb04cb1722f550e4ddbc17007943
KimEuiSoo <dmltn3426@naver.com>
2026-06-09 11:21:33 +0900

    feat: user 정보 조회 api 추가

--- app/app.py
+++ app/app.py
@@ -6,18 +6,17 @@ from app.core.config import settings
 from app.db.db import get_db, init_db
 from app.routes.auth.login import router as login_router
 from app.routes.auth.signup import router as signup_router
+from app.routes.user.user import router as user_router
 import os
 
-
-app = FastAPI()
 
 app = FastAPI(title=settings.PROJECT_NAME)
 app.include_router(login_router)
 app.include_router(signup_router)
+app.include_router(user_router)
 
 init_db()  # 애플리케이션 시작 시 DB 초기화 (테이블 생성 등)
 
-app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
