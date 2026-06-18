383264d6dbf4c85bb3b00ff32485af949ce8607e
Sungho <hurgj123kr@gmail.com>
2026-06-18 17:01:40 +0900

    [fix] config.py SUMMARIZE_MODEL 추가

--- app/core/config.py
+++ app/core/config.py
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
     OLLAMA_BASE_URL: str = "http://localhost:11434" #모델 url
     # 역할에 따라 모델 분리
     # str은 헌팅식 python 문법입니다. typescript처럼 타입을 미리 정의를 해주는방식
-    SUMMARIZE_MODEL: str = "exaone3.5:2.4b"
+    SUMMARIZE_MODEL: str = Field(default="exaone3.5:2.4b")
     EMBEDDING_MODEL: str = Field(default="BAAI/bge-m3")    # 임베딩용
     RAG_MODEL: str = "law-qwen-7b"          # RAG 응답용
     #임베딩모델 설정
